2fc917047ba6755d1437691218657f9a8a1821e0
nonju <albinzon@gmail.com>
2015-09-30 18:17:14 +0000

    working on second version of 'make_keystring'

--- encrypt.py
+++ encrypt.py
@@ -17,40 +17,6 @@ def seed_test(seed):
     
 #serious shit below
 
-def make_nr_inrange(nr, low=0, high=1):
-    """Makes a number fit a certain interval"""
-    
-    #sets number in interval_range
-    while True:
-        if nr > high:
-            nr -= high
-        elif nr < low:
-            nr += low
-        #else: #number is in range
-            #break
-        if nr < high and nr > low: #number is in range
-            break
-            
-    """            
-    if nr > high:
-        while nr > high:
-            nr -= high
-    if nr < low:
-        while nr < low:
-            nr += low
-   """ 
-    #extra saftey measure
-    if nr > high:
-        nr = high
-    elif nr < low:
-        nr = low
-    
-    return nr
-
-#def #turn keystring into capital_letters
-def numbers_to_keystring(number_list):
-    pass
-
 def load_primes(filename):
     """loads prime_numbers from a txt-file
     meant to load all primes every single 
@@ -68,13 +34,45 @@ def load_primes(filename):
         
     return prime_list
 
+def make_nr_inrange(nr, low=0, high=1):
+    """Makes a number fit a certain interval"""
+    
+    #sets number in interval_range
+    while True:
+        if nr > high:
+            nr -= high
+        elif nr < low:
+            nr += low
+        #else: #number is in range
+            #break
+        if nr < high and nr > low: #number is in range
+            break
+            
+    #extra saftey measure
+    if nr > high:
+        nr = high
+    elif nr < low:
+        nr = low
+    
+    return nr
+
+def numbers_to_keystring(number_list):
+    """turns a number into a char matching it's unicode value"""
+    
+    key_string = ''
+    for nr in number_list:
+        key_string += chr(nr)
+    
+    
+    return key_string
+
 def make_keystring(prime_list, pw):
-    """Creates a encoded keystring from a pw"""
+    """Creates an encoded Capital_Letter key_string from a pw"""
     
     number_list = [] #container for generated numbers
     for char in pw:
         #convert char to unicode
-        tmp_nr = ord(char)
+        tmp_nr = ord(char) 
         
         #get index of char, multiply tmp_nr by the
         #entry in prime_list that has that index
@@ -86,28 +84,57 @@ def make_keystring(prime_list, pw):
         #append nr to number_list
         number_list.append(combined_nr)
     
-    
-    key_string = '' #container for finished key_string
+    #container for finished key_string
     key_string = numbers_to_keystring(number_list)
     
-    #return key_string
-    return number_list
+    return key_string
+    #return number_list
 
-def encrypt_pw(pw, seed=' '):
+def make_keystring_v2(prime_list, pw):
+    """Creates an numeric number_list that later
+    is remade into a unicode char in a different
+    way than v.1"""
+    
+    key_string = ''
+    number_list = []
+    
+    #multiply unicode for each letter with matching prime_number (has same index)
+    for i in range(0, len(pw)):
+        tmp = pw[i]
+        tmp_add = ord(tmp) * int(prime_list[i])
+        #tmp_add -= prime_list[i+1]
+        number_list.append(tmp_add)
+    
+    number_string = ''.join(map(str, number_list)) #make number_list into one long number_string
+    #print(int(number_string))
+    
+    
+    return number_list
+    #return key_string
+
+def encrypt_pw(pw, seed=' ', prime_start=1, key_string_method=2):
     """Encrypts a password and returns it 
-    as a keystring
+    as a key_string
     """
     
     #get prime numbers
-    prime_list = load_primes('TextFiles/prime_numbers_short.txt')
+    tmp_prime_list = load_primes('TextFiles/prime_numbers_short.txt')
+    
+    #make a prime list starting at 'prime_start' (start at ex. the 100th prime_number)
+    prime_list = []
+    for i in range(prime_start, len(tmp_prime_list)):
+        prime_list.append(tmp_prime_list[i])
     
     #random primelist after seed
     random.seed(seed)
     prime_list = random.sample(prime_list, len(pw))
     
-    keystring = make_keystring(prime_list, pw)
+    if key_string_method == 1:
+        key_string = make_keystring(prime_list, pw)
+    elif key_string_method == 2:
+        key_string = make_keystring_v2(prime_list, pw)
     
-    return keystring
+    return key_string
     
     
     
--- main.py
+++ main.py
@@ -9,12 +9,28 @@ if __name__ == '__main__':
     
     #username = input('Input your username: ')
     password = input('Input your password: ')
+    seed = input('Enter a seed for your PW: ')
     
     #nr = encrypt.seed_test('')
     #print('nr:',nr)
     
     #enc_pw = encrypt.encrypt_pw('testPW', 'test')
-    enc_pw = encrypt.encrypt_pw(password, 'seed')
+    
+    enc_pw = ''
+    
+    while True:
+        selection = int(input('What encryption do you want to use? (1 or 2): '))
+        
+        if selection == 1:
+            enc_pw = encrypt.encrypt_pw(password, seed, prime_start=50, key_string_method=selection)
+            break
+        elif selection == 2:
+            enc_pw = encrypt.encrypt_pw(password, seed, prime_start=50, key_string_method=selection)
+            break
+        else:
+            print('Chose one of the available selections')
+        
+    
     
     print('encrypted password:', enc_pw)
 
